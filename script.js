f5955295f0d5dfd67503b13bd24f4a8263548203
ishsshukla <shukla22164024@akgec.ac.in>
2023-10-16 01:19:19 +0530

    enemies added

---
+++ script.js
@@ -0,0 +1,30 @@
+var hero = {
+      left: 575,
+      top: 700
+  };
+
+  var missiles = [];
+
+  var enemies = [
+     
+      { left: 200, top: 100 }, { left: 300, top: 100 },
+      { left: 400, top: 100 }, { left: 500, top: 100 },
+      { left: 600, top: 100 }, { left: 700, top: 100 },
+      { left: 800, top: 100 }, { left: 900, top: 100 },
+      { left: 1000, top: 100 },{ left: 1100, top: 100 },
+      { left: 1200, top: 100 },{ left: 1300, top: 100 },
+
+      { left: 200, top: 175 },{ left: 300, top: 175 },
+      { left: 400, top: 175 },{ left: 500, top: 175 },
+      { left: 600, top: 175 },{ left: 700, top: 175 },
+      { left: 800, top: 175 },{ left: 900, top: 175 },
+      { left: 1000, top: 175 },{ left: 1100, top: 175 },
+       { left: 1200, top: 175 },{ left: 1300, top: 175 },
+
+       { left: 200, top: 255 },{ left: 300, top: 255 },
+      { left: 400, top: 255 },{ left: 500, top: 255 },
+      { left: 600, top: 255 },{ left: 700, top: 255 },
+      { left: 800, top: 255 },{ left: 900, top: 255 },
+      { left: 1000, top: 255 },{ left: 1100, top: 255 },
+       { left: 1200, top: 255 },{ left: 1300, top: 255 },
+  ];
